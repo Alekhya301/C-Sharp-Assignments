f47ca8e3084700705a4b89e9964a9ca05416ee41
Alekhya301 <115077236+Alekhya301@users.noreply.github.com>
2022-11-11 20:18:38 +0530

    Add files via upload

---
+++ Assignment-5/ConsoleApp3/Assign-5(2).cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class EmployeeManagementSystem
+    {
+        public static void Main()
+        {
+           
+           ArrayList Employee = new ArrayList();
+            Employee.Add(101);
+            Employee.Add("Alekhya");
+            Employee.Add("Trainee");
+            Employee.Add(20000);
+            Employee.Add("Banglore");
+
+
+
+            foreach (object i in Employee)
+            {
+                Console.WriteLine(i + " ");
+                
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Assignment-5/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] alekhya = new int[5];
+            for(int i = 0; i < alekhya.Length; i++)
+            {
+                Console.WriteLine(" Enter array elements:",alekhya[i]);
+                Console.ReadLine();
+                
+            }
+            Console.WriteLine(" sorted array list :");
+            Array.Sort(alekhya);
+            foreach (int i in alekhya)
+            {
+                Console.WriteLine(i + " ");
+            }
+        }
+    }
+}
